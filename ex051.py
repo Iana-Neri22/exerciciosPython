0f7865edef754145bab486e7bd8c40dcf838435e
Iana Alexandre Neri <iananeri@outlook.com>
2021-02-25 12:55:23 -0300

    exercicio 51

--- ex051.py
+++ ex051.py
@@ -1,14 +1,17 @@
 primeiroTermo = int(input('Digite o primeiro termo da progressão aritmética: '))
 razao = int(input('Digite a razão da progressão aritmética: '))
-decimo = primeiroTermo + (10 -1) * razao
+contador = 1
+total = 0
+termo = primeiroTermo
+mais = 10
 
-'''
-for c in range(primeiroTermo, decimo + razao, razao):
-    print(primeiroTermo)
-    primeiroTermo += razao
-'''
-while not 
-    print(primeiroTermo)
-    primeiroTermo += razao
+while mais != 0:
+    total = total + mais
+    while contador < total:
+        print(termo)
+        termo += razao
+        contador += 1
 
+    mais = int(input('Quantos termos você quer mostrar a mais? '))
 
+print('Programa finalizado!')
