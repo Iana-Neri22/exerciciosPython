ee1b721fcc51431d65836944f0fd15af04f4bfd0
Iana Alexandre Neri <iananeri@outlook.com>
2021-02-25 21:37:48 -0300

    exercício 65

--- ex065.py
+++ ex065.py
@@ -1,4 +1,23 @@
-fechar = 'N'
+fechar = 'S'
+contadorNumeros = 0
+soma = 0
+maiorNumero = 0
+menorNumero = 0
 
-while fechar != 'S':
+while fechar != 'N':
     numero = int(input('Digite um número:'))
+    contadorNumeros += 1
+    soma += numero
+
+    if numero > maiorNumero:
+        maiorNumero = numero
+    if contadorNumeros == 1:
+        menorNumero = numero
+    elif numero < menorNumero:
+        menorNumero = numero
+
+    fechar = input('Quer continuar? [S/N]').upper().strip()
+
+media = soma / contadorNumeros
+print('Você digitou {} números e a média foi {}'.format(contadorNumeros, media))
+print('O maior valor foi {} e o menor foi {}'.format(maiorNumero, menorNumero))
