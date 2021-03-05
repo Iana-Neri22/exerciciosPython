73c3077266ea03152e58989d6e8eb2e6efdde31e
Iana Alexandre Neri <iananeri@outlook.com>
2021-03-05 18:23:02 -0300

    exercício 78

--- ex078.py
+++ ex078.py
@@ -1,34 +1,19 @@
-numbers = [int(input('Digite um número: '))
-    , int(input('Digite um número: '))
-    , int(input('Digite um número: '))
-    , int(input('Digite um número: '))
-    , int(input('Digite um número: '))]
+numbers = []
+
+for c in range(0, 5):
+    numbers.append(input('Digite um número: '))
 
 highestNumbers = []
 lowestNumbers = []
 positionsHigh = []
 positionsLow = []
 
-for c, number in enumerate(numbers):
-    if c == 0:
-        highestNumbers.append(number)
-        lowestNumbers.append(number)
-        positionsHigh.append(numbers.index(number))
-        positionsLow.append(numbers.index(number))
-    elif number > max(highestNumbers):
-        highestNumbers.remove(max(highestNumbers))
-        highestNumbers.append(number)
-        positionsHigh.remove(max(positionsHigh))
-        positionsHigh.append(number)
-    elif number == max(highestNumbers):
-        highestNumbers.append(number)
-        positionsHigh.append(number)
-    elif number < min(lowestNumbers):
-        lowestNumbers.remove(min(lowestNumbers))
-        lowestNumbers.append(number)
-    elif number == min(lowestNumbers):
-        lowestNumbers.append(number)
+print(f'O maior valor digitado foi {max(numbers)} na posição ', end='')
+for i, number in enumerate(numbers):
+    if number == max(numbers):
+        print(i, end=' ')
 
-
-print(f'O maior valor digitado foi {max(numbers)} na posição {positionsHigh}')
-print(f'O menor valor digitado foi {min(numbers)} na posição {numbers.index(min(numbers))}')
+print(f'\n O menor valor digitado foi {min(numbers)} na posição ', end='')
+for i, number in enumerate(numbers):
+    if number == min(numbers):
+        print(i, end=' ')
