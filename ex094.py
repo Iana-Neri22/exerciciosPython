598b41c49a90fd43b4cd1e2dda513a63f523ca53
Iana Alexandre Neri <iananeri@outlook.com>
2021-03-14 18:47:50 -0300

    exercício 94

--- ex094.py
+++ ex094.py
@@ -1,4 +1,28 @@
-input('Nome: ')
-input('Sexo [M/F]: ')
-input('Idade: ')
+pessoas = []
+continua = 'S'
+
+while continua == 'S':
+
+    pessoas.append({
+    'nome': input('Nome: '),
+    'sexo': input('Sexo [M/F]: ').strip().upper(),
+    'idade': int(input('Idade: '))
+    })
+
+    continua = input('Quer continuar [S/N]? ').strip().upper()
+    while continua != 'S' and continua != 'N':
+        continua = input('Quer continuar [S/N]? ').strip().upper()
+
+totalIdades = 0
+totalPessoas = pessoas.count(pessoas)
+mulheresCadastradas = []
+
+for pessoa in pessoas:
+    totalIdades += pessoa[]
+
+media = totalIdades / totalPessoas
+
+print(f'O grupo tem {totalPessoas} pessoas.')
+print(f'A média de idade é de {media} anos.')
+#print(f'As mulheres cadastradas foram {}')
 
